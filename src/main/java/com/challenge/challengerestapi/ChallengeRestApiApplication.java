56533ac4a3e6189fb42f33688c06a7c8c126b69d
Sara Lima <melissasara31@gmail.com>
2022-08-08 17:34:36 -0300

    Project created

---
+++ src/main/java/com/challenge/challengerestapi/ChallengeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.challengerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/challengerestapi/ChallengeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.challengerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
