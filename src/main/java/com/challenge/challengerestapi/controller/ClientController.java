abf705918a03797c46156b793effcb38f5bd0521
Sara Lima <melissasara31@gmail.com>
2022-08-09 18:00:27 -0300

    Implementing DELETE and UPDATE methods

--- src/main/java/com/challenge/challengerestapi/controller/ClientController.java
+++ src/main/java/com/challenge/challengerestapi/controller/ClientController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -35,6 +37,16 @@ public class ClientController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Client addClient(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	
+	@DeleteMapping("/client")
+	public void deleteClient(@RequestBody Client client) {
+		clientRepository.delete(client);
+	}
+	
+	@PutMapping("/client")
+	public Client updateClient(@RequestBody Client client) {
 		return clientRepository.save(client);
 	}
 }
