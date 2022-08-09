a6c9a2eb913d44d1c916a3089176b5e97f0f8623
Sara Lima <melissasara31@gmail.com>
2022-08-09 16:11:45 -0300

    Client Controller Created

---
+++ src/main/java/com/challenge/challengerestapi/controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.challenge.challengerestapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.challenge.challengerestapi.model.Client;
+import com.challenge.challengerestapi.repository.ClientRepository;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@GetMapping
+	public List<Client> listClients(){
+		return clientRepository.findAll();
+	}
+}
